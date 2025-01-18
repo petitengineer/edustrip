f879e781060a391b9b030c5acda11064d39a18b3
Nalan Kurnaz <nalan.kurnaz99@gmail.com>
2025-01-18 13:31:21 -0500

    Deletion

--- web/api.js
+++
@@ -1,5 +0,0 @@
-// Sets up the API client for interacting with your backend. 
-// For your API reference, visit: https://docs.gadget.dev/api/edustrip
-import { Client } from "@gadget-client/edustrip";
-
-export const api = new Client({ environment: window.gadgetConfig.environment });
